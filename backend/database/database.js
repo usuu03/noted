fee9c5f7cf81c03400780f9f839dc78ef07c017b
Usu <usuedeaghe@gmail.com>
2024-10-01 21:30:10 +0100

    setting server

---
+++ backend/database/database.js
@@ -0,0 +1,5 @@
+const mysql2 = require("mysql2")
+
+const db = mysql2.createConnection({
+    host: "localhost"
+})
---
+++ backend/server.js
@@ -0,0 +1,9 @@
+/*
+ * Filename: server.js
+ * Author: Usu Edeaghe
+ * Date: October 1, 2024
+ * Description: This file contains ....
+ *
+ */
+
+const express = require
